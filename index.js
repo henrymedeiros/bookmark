51bc04bd838d85e7d2896aaa39506b0a91a9b930
henrymedeiros <henrymedeiros77@gmail.com>
2022-10-15 19:40:53 -0300

    mobile features section and functional tabs

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const body = document.querySelector("body");
 const menu = document.querySelector("#mobileMenu");
 const header = document.querySelector("header");
 
+const tabs = document.querySelectorAll(".tab");
+const tabsContent = document.querySelectorAll(".tab-content");
+
 function toggleMenu() {
   menu.classList.toggle("hidden");
   body.classList.toggle("overflow-hidden");
   header.classList.toggle("text-white");
 }
+
+console.log(tabsContent);
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabsContent.forEach((tabContent) => {
+      tabContent.classList.remove("active-tab");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tabsContent[index].classList.add("active-tab");
+    tabs[index].classList.add("active-tab");
+  });
+});
